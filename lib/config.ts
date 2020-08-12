47953f3b196ed3e5a238811908b674f9c6333a34
Serhii <sergiybiluk@gmail.com>
2020-08-12 22:15:11 +0200

    remove password

--- lib/config.ts
+++ lib/config.ts
@@ -2,7 +2,7 @@ const config = {
     contactPoint: "mars-app-cosmosdb.cassandra.cosmos.azure.com",
     port: 10350,
     username: 'mars-app-cosmosdb',
-    password: 'cRJyZXjkuY2EpbtDX27ZzP9Cedesr3RYF4iniWBjexIr4VIAe24ue721VYcxIPjqyMK3NefEBLR3hlrE8wUXPg==',
+    password: '**',
     keyspace: 'likes',
     localDataCenter: 'North Europe'
 };
